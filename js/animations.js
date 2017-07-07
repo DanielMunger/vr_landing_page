fc9a4a75e6824f5265adf8cb83a84f825db60d44
DanielMunger <mungerda@gmail.com>
2017-07-06 17:34:36 -0700

    change js animation to trigger on top of div rather than scroll

--- js/animations.js
+++ js/animations.js
@@ -1,14 +1,24 @@
-var page = $(window)
+// Global Variables
 
+var page = $(window);
 
 // Window Scroll
+
 $(window).scroll(function() {
-  if ( page.scrollTop() >= 930 ) {
+  var divTop = $('#block-one').offset().top;
+  console.log(page.scrollTop());
+  if ( page.scrollTop() >= divTop ) {
     headerFadeIn();
   }
 
-  if ( page.scrollTop() < 930 ) {
+  if ( page.scrollTop() < divTop ) {
     headerFadeOut();
+  }
+  if ( page.scrollTop() > divTop ) {
+    iPhoneSlideIn();
+  }
+  if ( page.scrollTop() < divTop ) {
+    iPhoneSlideOut();
   }
 
 });
